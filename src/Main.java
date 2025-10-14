07cde4ffdc299cb3e91b5a592a0e5d7f11380417
thushan-123 <thushanmadusanka456@gmail.com>
2025-10-14 16:14:54 +0530

    create socket reciev response data

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,56 @@
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.InetAddress;
 
 public class Main {
     public static void main(String[] args) {
+        DatagramSocket serverSocket = null;
 
         try{
-            DatagramSocket serverSocket = new DatagramSocket(6767);
+            serverSocket = new DatagramSocket(6767);
             System.out.println("DHCP Server run on 0.0.0.0:67");
 
-            byte[] receivedData = new byte[1024];
-            DatagramPacket datagramPacket = new DatagramPacket(receivedData, receivedData.length);
+            while (true){
+                byte[] receivedData = new byte[1024]; // create buffer
+                DatagramPacket datagramPacket = new DatagramPacket(
+                        receivedData,
+                        receivedData.length
+                );
+
+                serverSocket.receive(datagramPacket);
+                System.out.println(
+                        new String(
+                                datagramPacket.getData(),
+                                0,
+                                datagramPacket.getLength()
+                        )
+                );
+
+                InetAddress clientAddress = datagramPacket.getAddress();
+                int port = datagramPacket.getPort();
+
+                String response = "hii i am DHCP";
+                byte[] responseData = response.getBytes();
+                DatagramPacket resPacket = new DatagramPacket(
+                        responseData,
+                        responseData.length,
+                        clientAddress,
+                        port
+                ); // create the response pkt
+
+                serverSocket.send(resPacket);
+
+
+            }
 
 
         }catch (Exception e){
-            System.out.println("Err : " + e);
-            System.exit(1); // err is occur exit program with 1
+            e.printStackTrace();
+        }finally {
+            if(serverSocket != null && !serverSocket.isClosed()){
+                serverSocket.close();
+            }
         }
     }
 }
