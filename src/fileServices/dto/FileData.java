6d8592a1c74dd9c0285fa6275063847d7e1b24d5
thushan-123 <thushanmadusanka456@gmail.com>
2025-12-02 10:08:43 +0530

    create dto file data

--- src/fileServices/dto/FileData.java
+++ src/fileServices/dto/FileData.java
@@ -1,4 +1,78 @@
 package fileServices.dto;
 
+import java.net.Inet4Address;
+
 public class FileData {
+    String poolId;
+    String poolName;
+    Inet4Address defaultGateway;
+    Inet4Address dns;
+    Inet4Address subnetMask;
+    Inet4Address networkIp;
+    int numberOfIp;
+
+    public String getPoolId() {
+        return poolId;
+    }
+
+    public void setPoolId(String poolId) {
+        this.poolId = poolId;
+    }
+
+    public String getPoolName() {
+        return poolName;
+    }
+
+    public void setPoolName(String poolName) {
+        this.poolName = poolName;
+    }
+
+    public Inet4Address getDefaultGateway() {
+        return defaultGateway;
+    }
+
+    public void setDefaultGateway(Inet4Address defaultGateway) {
+        this.defaultGateway = defaultGateway;
+    }
+
+    public Inet4Address getDns() {
+        return dns;
+    }
+
+    public void setDns(Inet4Address dns) {
+        this.dns = dns;
+    }
+
+    public Inet4Address getSubnetMask() {
+        return subnetMask;
+    }
+
+    public void setSubnetMask(Inet4Address subnetMask) {
+        this.subnetMask = subnetMask;
+    }
+
+    public Inet4Address getNetworkIp() {
+        return networkIp;
+    }
+
+    public void setNetworkIp(Inet4Address networkIp) {
+        this.networkIp = networkIp;
+    }
+
+    public int getNumberOfIp() {
+        return numberOfIp;
+    }
+
+    public void setNumberOfIp(int numberOfIp) {
+        this.numberOfIp = numberOfIp;
+    }
 }
+
+
+//# ip-pool  <pool-id> <pool-name> <network-ip> <subnet-mask> <start-pool-line-number> <number-of-ips>
+//        # default-gateway 192.168.1.1
+//        # dns 8.8.8.8
+//
+//        # ip sub_pool <sub-pool-id> <size>      // line number 5  size 10  (next-sub-pool-line 17 (start-line + (sub-pool-id * size) +2) 5 + (10 * 1) + 2 = 17
+//ff:56:55:55:34:aa:12  192.168.1.100  5  assign// <mac-address> <ip> <lease-time> <assign or available>
+//ff:56:55:55:34:aa:12  192.168.1.101  5  assign
