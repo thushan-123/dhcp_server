240c17615b2f17d1a10fdb9f3ff1f9f40c7fa598
thushan-123 <thushanmadusanka456@gmail.com>
2025-12-02 22:51:26 +0530

    implement methods

--- src/fileServices/DhcpFileService.java
+++ src/fileServices/DhcpFileService.java
@@ -1,4 +1,15 @@
 package fileServices;
 
-public class DhcpFileService {
+import fileServices.dto.FileData;
+
+public class DhcpFileService implements DhcpFileServiceRepository{
+    @Override
+    public boolean createFile(String filename) {
+        return false;
+    }
+
+    @Override
+    public FileData getDhcpValues(String filename) {
+        return null;
+    }
 }
