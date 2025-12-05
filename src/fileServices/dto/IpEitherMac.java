d5de06635fdc40576186930edac084f06dc94685
thushan-123 <thushanmadusanka456@gmail.com>
2025-12-05 16:21:22 +0530

    create getter setter method

--- src/fileServices/dto/IpEitherMac.java
+++ src/fileServices/dto/IpEitherMac.java
@@ -6,10 +6,27 @@ public class IpEitherMac {
     Inet4Address ip;
     String mac;
 
+    public IpEitherMac() {}
+
     public IpEitherMac(Inet4Address ip) {}
 
     public IpEitherMac(String mac) {}
 
     public IpEitherMac(Inet4Address ip, String mac) {}
 
+    public Inet4Address getIp() {
+        return ip;
+    }
+
+    public void setIp(Inet4Address ip) {
+        this.ip = ip;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
 }
