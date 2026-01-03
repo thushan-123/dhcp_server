732c66a0605989442b43f1bd7c11bf4f6d5bb63a
thushan-123 <thushanmadusanka456@gmail.com>
2026-01-03 11:39:27 +0530

    new class dhcp sorage

--- src/dhcpServices/dhcpStorage/DhcpStorage.java
+++ src/dhcpServices/dhcpStorage/DhcpStorage.java
@@ -1,4 +1,28 @@
 package dhcpServices.dhcpStorage;
 
-public interface DhcpStorage {
+import utility.IpPool;
+import utility.IpRecord;
+
+import java.net.Inet4Address;
+
+public class DhcpStorage implements  DhcpStorageServices{
+    @Override
+    public IpRecord generateIpRecord(IpRecord ipRecord) {
+        return null;
+    }
+
+    @Override
+    public IpRecord deleteIpRecord(Inet4Address inet4Address) {
+        return null;
+    }
+
+    @Override
+    public IpPool getIpPool() {
+        return null;
+    }
+
+    @Override
+    public void setIpPool(IpPool ipPool) {
+
+    }
 }
---
+++ src/dhcpServices/dhcpStorage/DhcpStorageServices.java
@@ -0,0 +1,18 @@
+package dhcpServices.dhcpStorage;
+
+import utility.IpPool;
+import utility.IpRecord;
+
+import java.net.Inet4Address;
+
+public interface DhcpStorageServices {
+
+    public IpRecord generateIpRecord(IpRecord ipRecord);
+
+    public IpRecord deleteIpRecord(Inet4Address inet4Address);
+
+    public IpPool getIpPool();
+
+    public void setIpPool(IpPool ipPool);
+
+}
