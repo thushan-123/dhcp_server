77452e405a5c5e53b940c19345307bde77f4c097
thushan-123 <thushanmadusanka456@gmail.com>
2026-02-20 17:56:33 +0530

    crete ne fn mac str convert to bytes

--- src/utility/MacUtility.java
+++ src/utility/MacUtility.java
@@ -1,4 +1,18 @@
 package utility;
 
+import javax.crypto.Mac;
+
 public class MacUtility {
+
+    public byte[] macToBytes(String mac) {
+        String[] strParts = mac.split(":");
+
+        byte[] macBytes = new byte[6];
+
+        for (int i = 0; i < strParts.length; i++) {
+            macBytes[i] = (byte) Integer.parseInt(strParts[i], 16);
+        }
+        return macBytes;
+    }
 }
+
